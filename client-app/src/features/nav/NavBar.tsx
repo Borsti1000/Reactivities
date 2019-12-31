84dad4faa5e83ec5d59bd75bc4a3e8703efb9f0f
Torsten Klinkhammer <torsten@Torstens-Mini-2.fritz.box>
2019-12-31 12:22:06 +0100

    End of Section 7

--- client-app/src/features/nav/NavBar.tsx
+++ client-app/src/features/nav/NavBar.tsx
@@ -1,11 +1,9 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { Menu, Container, Button } from 'semantic-ui-react';
+import ActivityStore from '../../app/stores/activityStore';
 
-interface IProps {
-    openCreateForm: () => void;
-}
-
-const NavBar : React.FC<IProps> = ({openCreateForm}) => {
+const NavBar : React.FC = () => {
+    const activityStore = useContext(ActivityStore);
     return (
         <div>
            <Menu fixed='top' inverted>
@@ -16,7 +14,7 @@ const NavBar : React.FC<IProps> = ({openCreateForm}) => {
                </Menu.Item>
                 <Menu.Item name='Activities'/>
                 <Menu.Item>
-                    <Button positive content='Create Activity' onClick={openCreateForm}/>
+                    <Button positive content='Create Activity' onClick={activityStore.openCreateForm}/>
                 </Menu.Item>
                </Container>                
             </Menu> 
